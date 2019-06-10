0a7cbd41a45d7182505ce00f15865481133af044
berngarciamadrid <bernabemadrid@gmail.com>
2019-06-10 12:54:27 +0200

    first commit NestJS MongoDB REST API Products

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost/nest-mongo-angular', {
+    useNewUrlParser: true,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
